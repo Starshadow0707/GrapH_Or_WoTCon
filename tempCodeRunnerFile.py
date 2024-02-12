0060299c85b6b77a3cfae0b6b3d13ba52f46ccb7
Patel HetKumar SandipBhai <129964953+Starshadow0707@users.noreply.github.com>
2024-02-12 20:32:54 +0530

    vxcv

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,10 +3,10 @@ import random
 
 def make_random_commits():
     total_commits = 0
-    while total_commits <= 1000:
+    while total_commits <= 2500:
         days = random.randint(1, 30)  # Randomly select days between 1 and 30
         total_commits += days
-        if total_commits > 1000:
+        if total_commits > 2500:
             break
         commit_with_days(days)
 
