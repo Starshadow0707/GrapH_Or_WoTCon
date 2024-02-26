4eb2f8311d60029b297ee8ae305484660f419fe1
Patel HetKumar SandipBhai <129964953+Starshadow0707@users.noreply.github.com>
2024-02-26 08:18:58 +0530

    update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,3 @@
-import os
-import random
-
-def make_commit(days: int):
-    if days < 1:
-        # git push
-        return os.system('git push')
-    else:
-        dates = f'{days} days ago'
-        
-        with open('data.txt', 'a') as file:
-            file.write(f'{dates}\n')
-            
-        os.system('git add data.txt')
-        os.system('git commit --date="'+dates+'" -m "Commit {days} days ago"') 
-        return days * make_commit(days-1)
-
 def make_random_commits():
     total_commits = 700
     while total_commits <= 2500:
@@ -23,18 +6,3 @@ def make_random_commits():
         if total_commits > 2500:
             break
         commit_with_days(days)
-
-def commit_with_days(days: int):
-    dates = f'{days} days ago'
-    
-    with open('data.txt', 'a') as file:
-        file.write(f'{dates}\n')
-        
-    os.system('git add data.txt')
-    os.system('git commit --date="'+dates+'" -m "Commit {days} days ago"')
-
-# Random commits
-make_random_commits()
-
-# Start the commit with the specified days
-make_commit(500)
